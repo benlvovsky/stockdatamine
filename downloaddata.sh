c61e65b8fc3a835b37f05ba6d07b0219903d04c6
Benjamin Lvovsky <ben@lvovsky.com>
2015-12-06 23:04:48 +1100

    loads stocks from last day in DB. Not FX. FX is ot be done

--- downloaddata.sh
+++ downloaddata.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#downloadlist="downloadstocklist_test.csv"
 downloadlist="downloadstocklist.csv"
 export PGPASSWORD='postgres'
 #psql -h localhost -U postgres -d postgres -c "TRUNCATE stocks"
@@ -11,9 +12,24 @@ IFS=,
 cat $INPUT | while read stockName
 do
 	echo "Downloading $stockName..."
-	maxdate=`psql -H -h localhost -U postgres -d postgres -c "select max(date) from stocks where stock='CBA.AX';"`;year=${maxdate:0:14};month=${maxdate:6:2};day=${maxdate:9:2};echo "${maxdate}--->${day}/${month}/${year}"
-	psql -h localhost -U postgres -d postgres -c "select date from stocks where stock='$stockName' order by date limit 1"
-	curl -o models/$stockName.csv http://real-chart.finance.yahoo.com/table.csv?s=$stockName\&a=10\&b=28\&c=1997\&d=11\&e=4\&f=2015\&g=d\&ignore=.csv
+	maxdate=`psql -t -h localhost -U postgres -d postgres -c "select max(date) from stocks where stock='$stockName';"`
+	maxdate=${maxdate//[[:blank:]]/} #trim
+	echo "maxdate='$maxdate'"
+	if [ -z "$maxdate" ]; then
+		maxdate="2014-01-01"
+	fi
+	echo "maxdate in use:$maxdate"
+	year=${maxdate:0:4}
+	month=${maxdate:5:2}
+	monthfixed=$((month-1))
+	printf "monthfixed=${monthfixed}\n"
+	day=${maxdate:8:2}
+	echo "Date to download from: ${maxdate}=${day}/${month}/${year}"
+	url="http://real-chart.finance.yahoo.com/table.csv?s=$stockName&a=${monthfixed}&b=${day}&c=${year}&d=11&e=4&f=2025&g=d&ignore=.csv"
+	echo $url
+	#http://real-chart.finance.yahoo.com/table.csv?s=CBA.AX\&a=11 &b=4&c=2015&d=11&e=6&f=2015&g=d&ignore=.csv
+	#http://real-chart.finance.yahoo.com/table.csv?s=CBA.AX\&a=12\&b=04\&c=2015\&d=11\&e=4\&f=2025\&g=d\&ignore=.csv
+	curl -o models/$stockName.csv ${url}
 	echo "Adding Code field..."
 	tail -n +2 models/${stockName}.csv | while read date open high low close vol adjclose tail
 	do
@@ -22,10 +38,9 @@ do
 	echo "Importing $stockName into DB from models/${stockName}_fixed.csv"
 	sql="COPY stocks (stock,date,open,high,low,close,\"Adj Close\") FROM '$(pwd)/models/${stockName}_fixed.csv' WITH CSV delimiter as ','"
 	printf "${sql}\n"
-	psql -h localhost -U postgres -d postgres -c "delete from stocks where stock='$stockName'"
+	#psql -h localhost -U postgres -d postgres -c "delete from stocks where stock='$stockName'"
 	psql -h localhost -U postgres -d postgres -c "${sql}"
 	echo "Importing $stockName into DB finished"
 done
 
 IFS=$OLDIFS
-
