abecaae0d1bc6f320584cd9e05f0a422e2bb27ce
Benjamin Lvovsky <ben@lvovsky.com>
2015-12-06 18:52:02 +1100

    works downloading all stocks and forex

--- downloadfxdata.sh
+++ downloadfxdata.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-downloadlist="downloadstocklist.csv"
+downloadlist="downloadfxlist.csv"
 export PGPASSWORD='postgres'
 #psql -h localhost -U postgres -d postgres -c "TRUNCATE stocks"
 
@@ -11,13 +11,11 @@ IFS=,
 cat $INPUT | while read stockName
 do
 	echo "Downloading $stockName..."
-	maxdate=`psql -H -h localhost -U postgres -d postgres -c "select max(date) from stocks where stock='CBA.AX';"`;year=${maxdate:0:14};month=${maxdate:6:2};day=${maxdate:9:2};echo "${maxdate}--->${day}/${month}/${year}"
-	psql -h localhost -U postgres -d postgres -c "select date from stocks where stock='$stockName' order by date limit 1"
-	curl -o models/$stockName.csv http://real-chart.finance.yahoo.com/table.csv?s=$stockName\&a=10\&b=28\&c=1997\&d=11\&e=4\&f=2015\&g=d\&ignore=.csv
+	curl -o models/$stockName.csv http://195.128.78.52/$stockName.csv?market=5\&em=181410\&code=$stockName\&df=1\&mf=10\&yf=2014\&from=01.11.2014\&dt=31\&mt=11\&yt=2025\&to=31.12.2025\&p=8\&f=$stockName\&e=.csv\&cn=$stockName\&dtf=1\&tmf=4\&MSOR=0\&mstimever=0\&sep=1\&sep2=3\&datf=5\&at=1
 	echo "Adding Code field..."
-	tail -n +2 models/${stockName}.csv | while read date open high low close vol adjclose tail
+	tail -n +2 models/${stockName}.csv | while read date time open high low close vol tail
 	do
-		printf "\"$stockName\",\"${date}\",\"${open}\",\"${high}\",\"${low}\",\"${close}\",\"${vol}\"\n"
+		printf "\"$stockName\",\"${date:0:4}-${date:4:2}-${date:6:2}\",\"${open}\",\"${high}\",\"${low}\",\"${close}\",\"${vol}\"\n"
 	done > models/${stockName}_fixed.csv
 	echo "Importing $stockName into DB from models/${stockName}_fixed.csv"
 	sql="COPY stocks (stock,date,open,high,low,close,\"Adj Close\") FROM '$(pwd)/models/${stockName}_fixed.csv' WITH CSV delimiter as ','"
@@ -26,6 +24,4 @@ do
 	psql -h localhost -U postgres -d postgres -c "${sql}"
 	echo "Importing $stockName into DB finished"
 done
-
 IFS=$OLDIFS
-
