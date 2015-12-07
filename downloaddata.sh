e8a14728940caa23e1df21a37c7a1843807fd432
Benjamin Lvovsky <ben@lvovsky.com>
2015-12-07 22:58:06 +1100

    noticed error if date from requested doesn't exist in yahoo finance. TODO: take care of error from yahoo finance or ignore if date is a future date from today

--- downloaddata.sh
+++ downloaddata.sh
@@ -13,9 +13,9 @@ do
 	echo "Downloading $stockName..."
 	rm models/${stockName}*.csv
 	maxDatePlusOne ${stockName}
-	url="http://real-chart.finance.yahoo.com/table.csv?s=$stockName&a=${monthfixed}&b=${day}&c=${year}&d=11&e=4&f=2025&g=d&ignore=.csv"
+	url="http://real-chart.finance.yahoo.com/table.csv?s=$stockName\&a=${monthfixed}\&b=${day}\&c=${year}\&d=11\&e=4\&f=2025\&g=d\&ignore=.csv"
 	echo $url
-	#http://real-chart.finance.yahoo.com/table.csv?s=CBA.AX\&a=12\&b=04\&c=2015\&d=11\&e=4\&f=2025\&g=d\&ignore=.csv
+	#http://real-chart.finance.yahoo.com/table.csv?s=CBA.AX&a=12&b=04&c=2015&d=11&e=4&f=2025&g=d&ignore=.csv
 	curl -o models/$stockName.csv ${url}
 	echo "Adding Code field..."
 	tail -n +2 models/${stockName}.csv | while read date open high low close vol adjclose tail
