e97f9d77f2490e330c357132848fee52fe1d49e8
Benjamin Lvovsky <ben@lvovsky.com>
2015-12-07 22:28:37 +1100

    works with incremental update for both stocks and forex

--- downloaddata.sh
+++ downloaddata.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
+source ./common.sh
 
 #downloadlist="downloadstocklist_test.csv"
 downloadlist="downloadstocklist.csv"
-export PGPASSWORD='postgres'
-#psql -h localhost -U postgres -d postgres -c "TRUNCATE stocks"
 
 INPUT=$downloadlist
 OLDIFS=$IFS
@@ -12,22 +11,9 @@ IFS=,
 cat $INPUT | while read stockName
 do
 	echo "Downloading $stockName..."
-	maxdate=`psql -t -h localhost -U postgres -d postgres -c "select max(date) from stocks where stock='$stockName';"`
-	maxdate=${maxdate//[[:blank:]]/} #trim
-	echo "maxdate='$maxdate'"
-	if [ -z "$maxdate" ]; then
-		maxdate="2014-01-01"
-	fi
-	echo "maxdate in use:$maxdate"
-	year=${maxdate:0:4}
-	month=${maxdate:5:2}
-	monthfixed=$((month-1))
-	printf "monthfixed=${monthfixed}\n"
-	day=${maxdate:8:2}
-	echo "Date to download from: ${maxdate}=${day}/${month}/${year}"
+	maxDatePlusOne ${stockName}
 	url="http://real-chart.finance.yahoo.com/table.csv?s=$stockName&a=${monthfixed}&b=${day}&c=${year}&d=11&e=4&f=2025&g=d&ignore=.csv"
 	echo $url
-	#http://real-chart.finance.yahoo.com/table.csv?s=CBA.AX\&a=11 &b=4&c=2015&d=11&e=6&f=2015&g=d&ignore=.csv
 	#http://real-chart.finance.yahoo.com/table.csv?s=CBA.AX\&a=12\&b=04\&c=2015\&d=11\&e=4\&f=2025\&g=d\&ignore=.csv
 	curl -o models/$stockName.csv ${url}
 	echo "Adding Code field..."
@@ -35,12 +21,12 @@ do
 	do
 		printf "\"$stockName\",\"${date}\",\"${open}\",\"${high}\",\"${low}\",\"${close}\",\"${vol}\"\n"
 	done > models/${stockName}_fixed.csv
-	echo "Importing $stockName into DB from models/${stockName}_fixed.csv"
-	sql="COPY stocks (stock,date,open,high,low,close,\"Adj Close\") FROM '$(pwd)/models/${stockName}_fixed.csv' WITH CSV delimiter as ','"
-	printf "${sql}\n"
-	#psql -h localhost -U postgres -d postgres -c "delete from stocks where stock='$stockName'"
-	psql -h localhost -U postgres -d postgres -c "${sql}"
-	echo "Importing $stockName into DB finished"
+#	echo "Importing $stockName into DB from models/${stockName}_fixed.csv"
+#	sql="COPY stocks (stock,date,open,high,low,close,\"Adj Close\") FROM '$(pwd)/models/${stockName}_fixed.csv' WITH CSV delimiter as ','"
+#	printf "${sql}\n"
+#	#	psql -h localhost -U postgres -d postgres -c "delete from stocks where stock='$stockName'"
+#	psql -h localhost -U postgres -d postgres -c "${sql}"
+#	echo "Importing $stockName into DB finished"
+	importIntoDB ${stockName}
 done
-
 IFS=$OLDIFS
--- downloadfxdata.sh
+++ downloadfxdata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
+source ./common.sh
 
 downloadlist="downloadfxlist.csv"
-export PGPASSWORD='postgres'
-#psql -h localhost -U postgres -d postgres -c "TRUNCATE stocks"
+#downloadlist="downloadfxlist_test.csv"
 
 INPUT=$downloadlist
 OLDIFS=$IFS
@@ -11,17 +11,18 @@ IFS=,
 cat $INPUT | while read stockName
 do
 	echo "Downloading $stockName..."
-	curl -o models/$stockName.csv http://195.128.78.52/$stockName.csv?market=5\&em=181410\&code=$stockName\&df=1\&mf=10\&yf=2014\&from=01.11.2014\&dt=31\&mt=11\&yt=2025\&to=31.12.2025\&p=8\&f=$stockName\&e=.csv\&cn=$stockName\&dtf=1\&tmf=4\&MSOR=0\&mstimever=0\&sep=1\&sep2=3\&datf=5\&at=1
+	#psql -h localhost -U postgres -d postgres -c "delete from stocks where stock='$stockName'"
+	maxDatePlusOne ${stockName}
+	url="http://195.128.78.52/${stockName}.csv?market=5\&em=181410\&code=${stockName}\&df=${day}\&mf=${monthfixed}\&yf=${year}\&from=${day}.${month}.${year}\&dt=31\&mt=11\&yt=2025\&to=31.12.2025\&p=8\&f=${stockName}\&e=.csv\&cn=${stockName}\&dtf=1\&tmf=4\&MSOR=0\&mstimever=0\&sep=1\&sep2=3\&datf=5\&at=1"
+	echo $url
+	curl -o models/$stockName.csv ${url}
+	#curl -o models/EURUSD.csv http://195.128.78.52/EURUSD.csv?market=5\&em=181410\&code=EURUSD\&df=4\&mf=11\&yf=2015\&from=04.12.2015\&dt=31\&mt=11\&yt=2025\&to=31.12.2025\&p=8\&f=EURUSD\&e=.csv\&cn=EURUSD\&dtf=1\&tmf=4\&MSOR=0\&mstimever=0\&sep=1\&sep2=3\&datf=5\&at=1
 	echo "Adding Code field..."
 	tail -n +2 models/${stockName}.csv | while read date time open high low close vol tail
 	do
-		printf "\"$stockName\",\"${date:0:4}-${date:4:2}-${date:6:2}\",\"${open}\",\"${high}\",\"${low}\",\"${close}\",\"${vol}\"\n"
+		volfixed=$(echo ${vol} | tr -d '\r')
+		printf "\"$stockName\",\"${date:0:4}-${date:4:2}-${date:6:2}\",\"${open}\",\"${high}\",\"${low}\",\"${close}\",\"${volfixed}\"\n"
 	done > models/${stockName}_fixed.csv
-	echo "Importing $stockName into DB from models/${stockName}_fixed.csv"
-	sql="COPY stocks (stock,date,open,high,low,close,\"Adj Close\") FROM '$(pwd)/models/${stockName}_fixed.csv' WITH CSV delimiter as ','"
-	printf "${sql}\n"
-	psql -h localhost -U postgres -d postgres -c "delete from stocks where stock='$stockName'"
-	psql -h localhost -U postgres -d postgres -c "${sql}"
-	echo "Importing $stockName into DB finished"
+	importIntoDB ${stockName}
 done
 IFS=$OLDIFS
