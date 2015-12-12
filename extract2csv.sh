0c53fcf850c399d80cf97fb8358f3360b230a26c
Benjamin Lvovsky <ben@lvovsky.com>
2015-12-12 13:16:46 +1100

    fixed recreation of folder and chmod

--- extract2csv.sh
+++ extract2csv.sh
@@ -16,13 +16,13 @@ INPUT="dataminestocklist.csv"
 OLDIFS=$IFS
 IFS=,
 [ ! -f $INPUT ] && { echo "$INPUT file not found"; exit 99; }
-rm -rf ./extract
-mkdir extracts
-#chmod -R 777 ./extracts
+rm -rf ./extracts
+mkdir ./extracts
+chmod 777 -R ./extracts
 cat $INPUT | while read stockName
 do
 	echo "Extracting $stockName..."
-	sql="COPY (select DATE,price,stockname,$(attributeList) FROM datamining_stocks_view where stockName='${stockName}') TO '$(pwd)/extracts/${stockName}.csv' DELIMITER ',' CSV HEADER"
+	sql="COPY (select DATE,price,$(attributeList) FROM datamining_stocks_view where stockName='${stockName}') TO '$(pwd)/extracts/${stockName}.csv' DELIMITER ',' CSV HEADER"
 	printf "${sql}\n"
 	psql -h localhost -U postgres -d postgres -c "${sql}"
 done
